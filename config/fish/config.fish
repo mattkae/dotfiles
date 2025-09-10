3f8a546503f7b7e869636cdbd8e336e7b453a8b8
Matt Kosarek <matt.kosarek@canonical.com>
2025-09-10 11:57:13 -0400

    Fixing bad export paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,7 @@ set -g theme_color_scheme dracula
 export MIR_SYMBOLS_MAP_GENERATOR_CLANG_SO_PATH=/usr/lib/llvm-19/lib/libclang-19.so.1
 export MIR_SYMBOLS_MAP_GENERATOR_CLANG_LIBRARY_PATH=/usr/lib/llvm-19/lib
 
-export PATH="$PATH:/home/matthew/Github/flutter/bin:/home/matthew/Programs/depot_tools"
+export PATH="$PATH:$HOME/.local/bin:$HOME/Github/flutter/bin:$HOME/Programs/depot_tools"
 
 # Export flutter as an environment variable
 export FLUTTER="$HOME/Github/flutter/bin/flutter"
