2fb85565199311bf0051288cf360e3d5ce22a9fb
Matt Kosarek <matt.kosarek@canonical.com>
2025-10-22 07:59:10 -0400

    Remove swww from install list

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ fi
 
 if $INSTALL_DEPS; then
   info "Installing applications dependencies from archive..."
-  sudo apt install atfs wofi swaylock swww bat fdfind kitty network-manager-gnome fish wlogout papirus-icon-theme pamixer brightnessctl sway-notification-center
+  sudo apt install atfs wofi swaylock bat fd-find kitty network-manager-gnome fish wlogout papirus-icon-theme pamixer brightnessctl sway-notification-center
 
   info "Installing development dependencis form archive..."
   sudo apt install golang
